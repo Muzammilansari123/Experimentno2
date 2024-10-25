42ec957d4b56db02e4f3638ec96b8752a34ff4e9
Muzammilansari123 <ansarimohdmuzammil46@eng.rizvi.edu.in>
2024-10-26 01:22:28 +0530

    Add files via upload

---
+++ expno02task2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,start_num,end_num,sum=0;i;
+    printf("\t\t\t***Sum Of Odd Number*** \n\n\n");
+    printf("Enter Start Number :");
+    scanf("%d",&start_num);
+    printf("Enter End Number : ");
+    scanf("%d",&end_num);
+    if(start_num>end_num)
+    {
+        printf("Error:- THE START NUMBER SHOULD BE LOWER THAN THE END NUMBER ");
+    }
+    else{
+
+     for(i=start_num;i<=end_num;i++)
+    {
+        if(i%2!=0){
+            sum=sum+i;
+        }
+
+    }
+
+    }
+    printf("sum of all the odd numbers is : %d",sum);
+    return 0;
+}
